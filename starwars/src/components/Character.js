ded9c8a036438f35077c07ddfe7e5c55270b1854
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-07-31 19:13:13 -0700

    styled; MVP

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
+import { NameTag, CardDiv, RoundImg, Txter } from './StyledComps';
 
 const Character = props => {
     const [character] = useState(props.character)
     return (
-        <div>
-            <h2>{character.name}</h2>
-            <img src={character.image} />
+        <CardDiv>
+            <NameTag>{character.name}</NameTag>
+            <RoundImg src={character.image} />
             <div className="character-info">
-                <p>Species: {character.species}</p>
-                <p>Gender: {character.gender}</p>
-                <p>Location: {character.location.name}</p>
-                <p>Status: {character.status}</p>
+                <Txter type="every">Species: {character.species}</Txter>
+                <Txter type="other">Gender: {character.gender}</Txter>
+                <Txter type="every">Location: {character.location.name}</Txter>
+                <Txter type="other">Status: {character.status}</Txter>
             </div>
-        </div>
+        </CardDiv>
     )
 };
 

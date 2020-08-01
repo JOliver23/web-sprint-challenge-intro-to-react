83f52cf45b397db280261a6e492d715835b84901
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-07-31 18:19:28 -0700

    imported needed dependencies to CharGrid; built a skelton of my functional component; commented logic; export default CharGrid

---
+++ starwars/src/components/CharGrid.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function CharGrid() {
+    const [apiData, setAPIData] = useState([])
+
+    return (
+        <div>
+            {/* {axios request; set response to state; map apiData; return Character.js component} */}
+        </div>
+    )
+};
+
+export default CharGrid;

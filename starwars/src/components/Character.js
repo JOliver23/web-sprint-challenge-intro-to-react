81a1a8636e95ed4bf589ed20e0ac117798a81ca2
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-07-31 18:25:37 -0700

    built character component w/ fake data place hlder; export default component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
+import React, { usestate } from 'react';
 
+const Character = props => {
+    // const [character] = useState({char.data.obj})
+    return (
+        <div>
+            <h2>character.name</h2>
+            <img src={character.img} />
+            <div className="character-info">
+                <p>age</p>
+                <p>info</p>
+                <p>more info</p>
+            </div>
+        </div>
+    )
+};
+
+export default Character;

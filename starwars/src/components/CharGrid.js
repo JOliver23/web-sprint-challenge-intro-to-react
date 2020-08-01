fea71efc42737b36e846f5be6de615848b97d945
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-07-31 18:35:25 -0700

    made api request in my useEffect; console.logged rsponse

--- starwars/src/components/CharGrid.js
+++ starwars/src/components/CharGrid.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Character } from './Character';
 
 function CharGrid() {
     const [apiData, setAPIData] = useState([])
 
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character/')
+        .then(response => console.log("api request: ", response))
+        .catch(err => console.log("error: ", err))
+    }, [])
+
     return (
         <div>
-            {/* {axios request; set response to state; map apiData; return Character.js component} */}
+            
         </div>
     )
 };
